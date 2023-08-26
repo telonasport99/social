526580988d95c3e6be2f9866b365fe82ff2687ef
telonasport99 <telonasport99@gmail.com>
2023-08-26 13:47:27 +0300

    13

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <header className={'header'}><img src={'https://yt3.googleusercontent.com/EPooXnprCPqEtU7DD6G2FGKkIdm7QTYwgFzmLobYBqbRNnJ1-FnCI1e1jFEWOuWAbjOn3CHpsA=s900-c-k-c0x00ffffff-no-rj'}/></header>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            Main content
+            <img src={'https://it-incubator.io/_next/static/media/logo.8a063c2a.svg'}/>
+            <div>
+                ava
+            </div>
+            <div>My posts
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
