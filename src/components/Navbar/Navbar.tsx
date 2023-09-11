690ba774dba4662df582507bf597c485651fa13b
telonasport99 <telonasport99@gmail.com>
2023-09-11 17:08:52 +0300

    20

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import s from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 const Navbar = () => {
     return (
         <div className={s.nav}>
             <nav >
-                <div><a href={'/profile'}>Profile</a></div>
-                <div><a href={'/dialogs'}>Messages</a></div>
-                <div><a href={'/news'}>News</a></div>
-                <div><a>Music</a></div>
-                <div><a>Settings</a></div>
+                <div className={s.item}><NavLink activeClassName={s.activeLink} to={'/profile'}>Profile</NavLink></div>
+                <div className={s.item}><NavLink activeClassName={s.activeLink} to={'/dialogs'}>Messages</NavLink></div>
+                <div className={s.item}><NavLink activeClassName={s.activeLink} to={'/news'}>News</NavLink></div>
+                <div className={s.item}><a>Music</a></div>
+                <div className={s.item}><a>Settings</a></div>
             </nav>
         </div>
     );
