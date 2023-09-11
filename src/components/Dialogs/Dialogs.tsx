fe0a2f791e291c43d031f0a89fff59fc9058189a
telonasport99 <telonasport99@gmail.com>
2023-09-11 15:53:43 +0300

    18

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import s from  'Dialogs.module.css';
+ type DialogsPropsType={
+
+ }
+const Dialogs = (props:DialogsPropsType) => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Dialogs;
