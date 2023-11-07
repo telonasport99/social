0d91b6e441ed685e611844ab3e29695d21315d7a
telonasport99 <telonasport99@gmail.com>
2023-11-07 16:05:05 +0300

    54

--- src/components/Users/UsersC.tsx
+++ src/components/Users/UsersC.tsx
@@ -2,8 +2,7 @@ import React from 'react'
 import {UserPropsType} from "./UserContainer";
 import axios from "axios";
 export class UsersC extends React.Component<UserPropsType>{
-    constructor(props: UserPropsType) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(res=>this.props.setUsers(res.data.items))
     }
