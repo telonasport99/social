23fbfdb21e57469ad18cb272cde8d1afaa5d4008
telonasport99 <telonasport99@gmail.com>
2023-11-07 13:37:49 +0300

    52

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,15 +5,17 @@ import axios from "axios";
 
 const Users = (props:UserPropsType) => {
 
-    if(props.usersPage.users.length===0){
+ let getUser=()=>{
+     if(props.usersPage.users.length===0){
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(res=>props.setUsers(res.data.items))
         /* props.setUsers(  [
          {id: 1,img:'https://www.shareicon.net/data/512x512/2015/10/17/657343_cat_512x512.png',followed:true, fullName: 'Vadim', status:'Work',location:{city:'Volovysk',country:'Belarus'}},
          {id: 2,img:'https://www.shareicon.net/data/512x512/2015/10/17/657343_cat_512x512.png',followed:false, fullName: 'Sasha', status:'Dont work',location:{city:'Hrodno',country:'Belarus'}},
-     ])*/}
+     ])*/}}
     return (
         <div>
+            <button onClick={getUser}>getUser</button>
             {props.usersPage.users.map(el => (
                 <div key={el.id}>
                   <img src={el.photos.small?el.photos.small:'https://www.shareicon.net/data/512x512/2015/10/17/657343_cat_512x512.png'}/>
